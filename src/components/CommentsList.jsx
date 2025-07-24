bc335f25c240140d62cfd1679f57dc8fab7f3318
KevR98 <ramil.kevin98@gmail.com>
2025-07-24 16:57:45 +0200

    adds comments

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -7,6 +7,7 @@ const CommentsList = (props) => {
       {props.content.slice(0, 1).map((comment) => {
         return (
           <ListGroup.Item key={comment._id}>
+            {/* Ho chiamato comment che lo uso in SingleComment = a comment (che si trova nell'array dell API) */}
             <SingleComment comment={comment} />
           </ListGroup.Item>
         );
--- src/components/SingleComment.jsx
+++ src/components/SingleComment.jsx
@@ -1,6 +1,7 @@
 const SingleComment = (props) => {
   return (
     <>
+      {/* il codice vuol dire che passo il props di comment(dichiarato in CommentsList).author */}
       <strong>{props.comment.author}: </strong>
       {props.comment.comment}
     </>
